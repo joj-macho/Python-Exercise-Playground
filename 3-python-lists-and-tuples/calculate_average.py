3608f33a4b8909f5a6ffc08f92aefca42c0cb28e
Joj Macho <zingakay@gmail.com>
2024-08-01 19:44:18 +0200

    Rename exercise to calculate_

---
+++ 3-python-lists-and-tuples/calculate_average.py
@@ -0,0 +1,20 @@
+'''
+Exercise: Calculating Average of Numbers in a List
+
+This script calculates the average of numbers in a given list.
+
+Usage:
+    Call the function with the list of numbers to get the average.
+
+Tags: loop, iteration, arithmetic operations, list manipulation
+'''
+
+def main():
+    '''Calculate the average of numbres in a list.'''
+    numbers = [2, 4, 6, 8, 10]
+    print(f'List of numbers: {numbers}:')
+    print(f'Average = {calculate_average(numbers)}')
+
+
+if __name__ == '__main__':
+    main()
